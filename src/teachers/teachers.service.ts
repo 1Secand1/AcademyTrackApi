eeffe369053b77b6aa3fb4cf9e3e938d5426917b
boevvv <boevvv@eastmining.ru>
2025-01-02 23:08:04 +0300

    feat: update

--- src/teachers/teachers.service.ts
+++ src/teachers/teachers.service.ts
@@ -7,31 +7,76 @@ import { PrismaService } from 'src/prisma.service';
 export class TeachersService {
   constructor(protected readonly prisma: PrismaService) {}
 
-  create(createTeacherDto: CreateTeacherDto) {
-    return this.prisma.teachers.create({
+  // TODO заменить на юзер ДТО
+  async create(createTeacherDto: CreateTeacherDto) {
+    const user = await this.prisma.user.create({
       data: createTeacherDto,
     });
+
+    return this.prisma.teacher.create({ data: { id: user.id } });
   }
 
-  findAll() {
-    return this.prisma.teachers.findMany();
-  }
-
-  findOne(id: number) {
-    return this.prisma.teachers.findUnique({
-      where: { id: id },
+  async findAll() {
+    const teachers = await this.prisma.teacher.findMany({
+      include: {
+        user: {
+          select: {
+            name: true,
+            surname: true,
+            patronymic: true,
+          },
+        },
+      },
     });
+
+    return teachers.map(({ user, ...teacher }) => ({
+      id: teacher.id,
+      ...user,
+    }));
   }
 
-  update(id: number, updateTeacherDto: UpdateTeacherDto) {
-    return this.prisma.teachers.update({
-      where: { id: id },
-      data: updateTeacherDto,
+  async findOne(id: number) {
+    const teacher = await this.prisma.teacher.findUnique({
+      where: { id },
+      include: {
+        user: {
+          select: {
+            name: true,
+            surname: true,
+            patronymic: true,
+          },
+        },
+      },
     });
+
+    return { ...teacher, ...teacher.user, user: undefined };
+  }
+
+  async update(id: number, updateTeacherDto: UpdateTeacherDto) {
+    const teacher = await this.prisma.teacher.update({
+      where: { id },
+      data: {
+        user: {
+          update: updateTeacherDto,
+        },
+      },
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+            surname: true,
+            patronymic: true,
+          },
+        },
+      },
+    });
+
+    return { ...teacher, ...teacher.user, user: undefined };
   }
 
   remove(id: number) {
-    return this.prisma.teachers.delete({
+    return this.prisma.teacher.delete({
       where: { id: id },
     });
   }
