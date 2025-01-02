3273060ccd25b1d3a99c896e17ae7619a937522e
boevvv <boevvv@eastmining.ru>
2025-01-02 23:07:02 +0300

    feat: update create-teacher.dto.ts

--- src/teachers/dto/create-teacher.dto.ts
+++ src/teachers/dto/create-teacher.dto.ts
@@ -1,6 +1,21 @@
-import { IsString } from 'class-validator';
+import { IsNumber, IsString } from 'class-validator';
 
 export class CreateTeacherDto {
+  @IsNumber()
+  id: number;
+
   @IsString()
   name: string;
+
+  @IsString()
+  patronymic: string;
+
+  @IsString()
+  surname: string;
+
+  @IsString()
+  login: string;
+
+  @IsString()
+  password: string;
 }
