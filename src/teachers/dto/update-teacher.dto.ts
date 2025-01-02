fa14fef56f3cca879ee1376fc34c0a8cddedfcd3
boevvv <boevvv@eastmining.ru>
2025-01-02 23:08:17 +0300

    feat: update update-teacher.dto.ts

--- src/teachers/dto/update-teacher.dto.ts
+++ src/teachers/dto/update-teacher.dto.ts
@@ -1,4 +1,17 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTeacherDto } from './create-teacher.dto';
+import { IsNumber, IsString } from 'class-validator';
 
-export class UpdateTeacherDto extends PartialType(CreateTeacherDto) {}
+export class UpdateTeacherDto extends PartialType(CreateTeacherDto) {
+  @IsNumber()
+  id: number;
+
+  @IsString()
+  name: string;
+
+  @IsString()
+  patronymic: string;
+
+  @IsString()
+  surname: string;
+}
