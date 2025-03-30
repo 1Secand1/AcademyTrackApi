b5008038c0cf8b78fbcb0bd903acdb502d65a568
boevvv <boevvv@eastmining.ru>
2025-03-30 14:16:56 +0300

    feat: change type date from string to date

--- src/schedule/dto/create-schedule.dto.ts
+++ src/schedule/dto/create-schedule.dto.ts
@@ -1,11 +1,13 @@
-import { IsInt, IsNumber, IsString } from 'class-validator';
+import { IsDate, IsInt, IsNumber } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class CreateScheduleDto {
   @IsInt()
   lessonNumber: number;
 
-  @IsString()
-  date: string;
+  @Type(() => Date)
+  @IsDate()
+  date: Date;
 
   @IsNumber()
   teacherGroupSubjectId: number;
